9fcc0c1432818de7b6574a06c5fdec296fde7f48
Drew King <drew.stephen.king@knights.ucf.edu>
2021-02-03 04:22:55 -0500

    added labels to screen

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import time
 import random
 
+pygame.font.init()
+
 #Define the width and height for pygame window
 WIDTH, HEIGHT = 750, 750
 
@@ -35,10 +37,22 @@ def main():
 	FPS = 60
 	clock = pygame.time.Clock()
 
+	level = 1
+	lives = 5
+	main_font = pygame.font.SysFont("comicsans", 50)
+
 	#handles drawing of window
 	def redraw_window():
 		#blit adds image to window at defined location (0,0)->top left in pygame
 		WIN.blit(BACKGROUND, (0,0))
+
+		#drawing text
+		lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+		level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+		#Add labels to screen
+		WIN.blit(lives_label, (10, 10))
+		WIN.blit(level_label, (WIDTH-level_label.get_width() - 10, 10))
 		pygame.display.update()
 
 	while run:

fd22c3a7a79acfd9400f8803f4cedba553cd67fe
Drew King <drew.stephen.king@knights.ucf.edu>
2021-02-03 03:38:49 -0500

    added assets to main.py

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import os
 import time
 import random
 
+#Load Assets
+BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+PIXEL_LASER_BLUE = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+PIXEL_LASER_GREEN = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+PIXEL_LASER_RED = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+PIXEL_LASER_YELLOW = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+PIXEL_SPACE_SHIP_BLUE = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+PIXEL_SPACE_SHIP_GREEN = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+PIXEL_SPACE_SHIP_RED = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+
+MAIN_PLAYER = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+

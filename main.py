a5a3359ad48b9c82cfd9192538445605e0cbde19
Drew King <drew.stephen.king@knights.ucf.edu>
2021-02-04 00:15:53 -0500

    define methods for enemy

--- main.py
+++ main.py
@@ -63,6 +63,30 @@ class Ship:
 	def draw(self, window):
 		#pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
 		window.blit(self.ship_img, (self.x, self.y))
+		for laser in self.lasers:
+			laser.draw(window)
+
+	def move_lasers(self, velocity, obj):
+		self.cooldown()
+		for laser in self.lasers:
+			laser.move(velocity)
+			if laser.off_screen(HEIGHT):
+				self.lasers.remove(laser)
+			elif laser.collision(obj):
+				obj.health -= 10
+				self.lasers.remove(laser)
+
+	def cooldown(self):
+		if self.cool_down_counter >= self.COOLDOWN:
+			self.cool_down_counter = 0
+		elif self.cool_down_counter > 0:
+			self.cool_down_counter += 1
+
+	def shoot(self):
+		if self.cool_down_counter == 0:
+			laser = Laser(self.x, self.y, self.laser_img)
+			self.lasers.append(laser)
+			self.cool_down_counter = 1
 
 	def get_width(self):
 		return self.ship_img.get_width()

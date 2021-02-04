7de73ca213a68933c2f3bdb55e7de32ce2dbceb6
Drew King <drew.stephen.king@knights.ucf.edu>
2021-02-04 00:58:28 -0500

    add title screen

--- main.py
+++ main.py
@@ -255,7 +255,19 @@ def main():
 				lives -= 1
 				enemies.remove(enemy)
 
-
-
 		player.move_lasers(-laser_velocity, enemies)
+
+def main_menu():
+	title_font = pygame.font.SysFont("comicsans", 70)
+	run = True
+	while run:
+		WIN.blil(BACKGROUND, (0, 0))
+		title_label = title_font.render("Press the mouse to being...", 1, (255, 255, 255))
+		WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				main()
+	pygame.quit()
 main()
